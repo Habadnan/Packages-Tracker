a586f084ff96574e708facb294b7ed04ce1c25f4
JazzyMrDerp <jamehta19@gmail.com>
2025-11-24 16:30:28 -0500

    11/24/25 - Changed HelloApplication.java so it can actually launch

--- src/main/java/org/example/trackit/HelloApplication.java
+++ src/main/java/org/example/trackit/HelloApplication.java
@@ -2,6 +2,7 @@ package org.example.trackit;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -18,12 +19,22 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        fstore = contextFirebase.firebase();
-        fauth = FirebaseAuth.getInstance();
+        FXMLLoader masterLoader = new FXMLLoader(getClass().getResource("master-page.fxml"));
+        Parent masterRoot = masterLoader.load();
+        MasterPageController masterController = masterLoader.getController();
+        masterRoot.setUserData(masterController);
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader contentLoader = new FXMLLoader(getClass().getResource("main-page.fxml"));
+        Parent content = contentLoader.load();
+        masterController.setContent(content);
+
+        Scene scene = new Scene(masterRoot, 1200, 800);
+
+        // Attach the CSS stylesheet here
+        scene.getStylesheets().add(
+                getClass().getResource("styles.css").toExternalForm()
+        );
+
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
